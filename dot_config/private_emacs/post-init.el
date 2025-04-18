cffcfab928810318ae517e8adca25d86452403de
Frestein <frestein@tuta.io>
2025-04-18 15:10:38 +0500

    feat(emacs): add treesit-language-source-alist

--- dot_config/private_emacs/post-init.el
+++ dot_config/private_emacs/post-init.el
@@ -48,6 +48,40 @@
 ;; the precise point where you previously left off.
 (add-hook 'after-init-hook #'save-place-mode)
 
+(setq treesit-language-source-alist
+	  '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+		(c "https://github.com/tree-sitter/tree-sitter-c")
+		(cmake "https://github.com/uyha/tree-sitter-cmake")
+		(common-lisp "https://github.com/theHamsta/tree-sitter-commonlisp")
+		(cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+		(css "https://github.com/tree-sitter/tree-sitter-css")
+		(csharp "https://github.com/tree-sitter/tree-sitter-c-sharp")
+		(elisp "https://github.com/Wilfred/tree-sitter-elisp")
+		(go "https://github.com/tree-sitter/tree-sitter-go")
+		(go-mod "https://github.com/camdencheek/tree-sitter-go-mod")
+		(html "https://github.com/tree-sitter/tree-sitter-html")
+		(js . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
+		(json "https://github.com/tree-sitter/tree-sitter-json")
+		(lua "https://github.com/Azganoth/tree-sitter-lua")
+		(make "https://github.com/alemuller/tree-sitter-make")
+		(markdown "https://github.com/ikatyang/tree-sitter-markdown")
+		(python "https://github.com/tree-sitter/tree-sitter-python")
+		(r "https://github.com/r-lib/tree-sitter-r")
+		(rust "https://github.com/tree-sitter/tree-sitter-rust")
+		(toml "https://github.com/tree-sitter/tree-sitter-toml")
+		(tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
+		(typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
+		(typst "https://github.com/uben0/tree-sitter-typst")
+		(yaml "https://github.com/ikatyang/tree-sitter-yaml")
+		(nix "https://github.com/nix-community/tree-sitter-nix")))
+
+(use-package treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+
 ;; Required by evil-collection
 (eval-when-compile
   ;; It has to be defined before evil
@@ -289,13 +323,6 @@
    ;; :preview-key "M-."
    :preview-key '(:debounce 0.4 any))
   (setq consult-narrow-key "<"))
-
-(use-package treesit-auto
-  :custom
-  (treesit-auto-install 'prompt)
-  :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode))
 
 (use-package treesit-fold)
 
