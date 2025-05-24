ec1cfc6030c804824c51df196b5387160f19205d
Frestein <frestein@tuta.io>
2025-05-24 11:17:33 +0500

    feat(fish): doom abbrs, refactor

--- dot_config/private_fish/conf.d/abbrs.fish
+++ dot_config/private_fish/conf.d/abbrs.fish
@@ -2,21 +2,27 @@ if not status is-interactive && test "$CI" != true
     exit
 end
 
-abbr -a -- a ansible
+# better defaults
 abbr -a -- bc bc -q
-abbr -a -- btmb btm -b --hide_avg_cpu --theme gruvbox
-abbr -a -- cz chezmoi
-abbr -a -- d docker
-abbr -a -- ddg BROWSER=links ddgr
-abbr -a -- news newsboat
-abbr -a -- sc systemctl
-abbr -a -- scu systemctl --user
+abbr -a -- btm btm -b --hide_avg_cpu --theme gruvbox
+
+# network
 abbr -a -- speedtest speedtest-go
-abbr -a -- y yazi
+
+# trans
+abbr -a -- t trans :ru
+abbr -a -- tl trans :ru --shell --brief
+abbr -a -- tt trans :en
+abbr -a -- ttl trans :en --shell --brief
+
+# share files
 abbr -a -- tb nc termbin.com 9999
+
+# fetch
 abbr -a -- f fastfetch
 abbr -a -- ff fastfetch -c $XDG_CONFIG_HOME/fastfetch/config-full.jsonc
 
+# eza
 set EZA_DEFAULTS --group --group-directories-first
 
 abbr -a -- lD eza -lD $EZA_DEFAULTS
@@ -31,19 +37,42 @@ abbr -a -- ls eza $EZA_DEFAULTS
 abbr -a -- lsd eza -d $EZA_DEFAULTS
 abbr -a -- lsdl eza -dl $EZA_DEFAULTS
 
-abbr -a -- t trans :ru
-abbr -a -- tl trans :ru --shell --brief
-abbr -a -- tt trans :en
-abbr -a -- ttl trans :en --shell --brief
+set -e EZA_DEFAULTS
 
+# yazi
+abbr -a -- y yazi
+
+# nvim
 abbr -a -- v nvim
 abbr -a -- vd nvim -d
 abbr -a -- vz nvim -c \'Telescope zoxide list\'
 abbr -a -- vc nvim -c \'Telescope chezmoi find_files\'
 
+# emacs
 abbr -a -- ec emacsclient -nc
 abbr -a -- ew emacsclient -nw
+abbr -a -- ds doom sync
+abbr -a -- dupd doom update
+abbr -a -- dupg doom upgrade
+abbr -a -- ddoc doom doctor
+abbr -a -- dpur doom purge
 
+# chezmoi
+abbr -a -- cz chezmoi
+abbr -a -- czdf chezmoi diff
+abbr -a -- czapl chezmoi apply
+
+# ansbile
+abbr -a -- a ansible
+
+# docker
+abbr -a -- d docker
+
+# systemd
+abbr -a -- sc systemctl
+abbr -a -- scu systemctl --user
+
+# archlinux
 abbr -a -- pacupg doas pacman -Syu
 abbr -a -- pacin doas pacman -S
 abbr -a -- paclean doas pacman -Sc
