010e62b1b45757aab003bbea7c5b0f4a6f3eb311
Frestein <frestein@tuta.io>
2025-05-27 02:38:52 +0500

    feat(firefox): add betterfox

--- private_dot_mozilla/private_firefox/private_arkenfox.default/user-overrides.js
+++ private_dot_mozilla/private_firefox/private_arkenfox.default/user-overrides.js
@@ -1,3 +1,9 @@
+/****************************************************************************
+ * Arkenfox                                                                 *
+ * version: 135                                                             *
+ * url: https://github.com/arkenfox/user.js                                 *
+****************************************************************************/
+
 // 0102: resume previous session
 user_pref("browser.startup.page", 3);
 
@@ -7,9 +13,6 @@ user_pref("browser.safebrowsing.phishing.enabled", false);
 
 // 0402: disable SB checks for downloads (both local lookups + remote)
 user_pref("browser.safebrowsing.downloads.enabled", false);
-
-// 0403: disable SB checks for downloads (remote)
-user_pref("browser.safebrowsing.downloads.remote.enabled", false);
 
 // 0404: disable SB checks for unwanted software
 user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
@@ -27,9 +30,6 @@ user_pref("browser.formfill.enable", true);
 
 // 1003: enable storing extra session data everywhere
 user_pref("browser.sessionstore.privacy_level", 0);
-
-// 1211: disable OCSP
-user_pref("security.OCSP.enabled", 0);
 
 // 2651: disable user interaction for asking where to download
 user_pref("browser.download.useDownloadDir", true);
@@ -52,3 +52,117 @@ user_pref("privacy.clearHistory.formdata", false);
 
 // 5003: disable saving passwords
 user_pref("signon.rememberSignons", false);
+
+
+/****************************************************************************
+ * Betterfox                                                                *
+ * "Ad meliora"                                                             *
+ * version: 137                                                             *
+ * url: https://github.com/yokoffing/Betterfox                              *
+****************************************************************************/
+
+/****************************************************************************
+ * SECTION: FASTFOX                                                         *
+****************************************************************************/
+/** GENERAL ***/
+user_pref("content.notify.interval", 100000);
+
+/** GFX ***/
+user_pref("gfx.canvas.accelerated.cache-size", 512);
+user_pref("gfx.content.skia-font-cache-size", 20);
+
+/** MEDIA CACHE ***/
+user_pref("media.cache_readahead_limit", 7200);
+user_pref("media.cache_resume_threshold", 3600);
+
+/** IMAGE CACHE ***/
+user_pref("image.mem.decode_bytes_at_a_time", 32768);
+
+/** NETWORK ***/
+user_pref("network.http.max-connections", 1800);
+user_pref("network.http.max-persistent-connections-per-server", 10);
+user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
+user_pref("network.http.pacing.requests.enabled", false);
+user_pref("network.dnsCacheExpiration", 3600);
+user_pref("network.ssl_tokens_cache_capacity", 10240);
+
+/** EXPERIMENTAL ***/
+user_pref("layout.css.grid-template-masonry-value.enabled", true);
+
+/****************************************************************************
+ * SECTION: SECUREFOX                                                       *
+****************************************************************************/
+/** OCSP & CERTS / HPKP ***/
+user_pref("security.OCSP.enabled", 0);
+
+/** DISK AVOIDANCE ***/
+user_pref("browser.sessionstore.interval", 60000);
+
+/** SHUTDOWN & SANITIZING ***/
+user_pref("browser.privatebrowsing.resetPBM.enabled", true);
+user_pref("privacy.history.custom", true);
+
+/** SEARCH / URL BAR ***/
+user_pref("browser.urlbar.trimHttps", true);
+user_pref("browser.urlbar.untrimOnUserInteraction.featureGate", true);
+user_pref("browser.urlbar.update2.engineAliasRefresh", true);
+user_pref("browser.urlbar.groupLabels.enabled", false);
+
+/** PASSWORDS ***/
+user_pref("signon.privateBrowsingCapture.enabled", false);
+user_pref("editor.truncate_user_pastes", false);
+
+/** SAFE BROWSING ***/
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+
+/** MOZILLA ***/
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("permissions.default.geo", 2);
+user_pref("geo.provider.network.url", "https://beacondb.net/v1/geolocate");
+user_pref("browser.search.update", false);
+
+/****************************************************************************
+ * SECTION: PESKYFOX                                                        *
+****************************************************************************/
+/** MOZILLA UI ***/
+user_pref("browser.privatebrowsing.vpnpromourl", "");
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("browser.aboutwelcome.enabled", false);
+user_pref("browser.profiles.enabled", true);
+
+/** THEME ADJUSTMENTS ***/
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.compactmode.show", true);
+
+/** FULLSCREEN NOTICE ***/
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("full-screen-api.warning.timeout", 0);
+
+/** URL BAR ***/
+user_pref("browser.urlbar.unitConversion.enabled", true);
+user_pref("dom.text_fragments.create_text_fragment.enabled", true);
+
+/** NEW TAB PAGE ***/
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+
+/** POCKET ***/
+user_pref("extensions.pocket.enabled", false);
+
+/** PDF ***/
+user_pref("browser.download.open_pdf_attachments_inline", true);
+
+/** TAB BEHAVIOR ***/
+user_pref("browser.bookmarks.openInTabClosesMenu", false);
+user_pref("browser.menu.showViewImageInfo", true);
+user_pref("findbar.highlightAll", true);
+user_pref("layout.word_select.eat_space_to_next_word", false);
+
+/****************************************************************************
+ * SECTION: SMOOTHFOX                                                       *
+****************************************************************************/
+
+/** INSTANT SCROLLING (SIMPLE ADJUSTMENT) ***/
+user_pref("general.smoothScroll", true); // DEFAULT
+user_pref("mousewheel.default.delta_multiplier_y", 275); // 250-400; adjust this number to your liking
