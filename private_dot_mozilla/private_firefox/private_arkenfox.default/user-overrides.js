ba878f8eac91789a52946af34e1a29c5e454c270
Frestein <frestein@tuta.io>
2025-06-13 17:36:33 +0500

    feat(firefox): enable quic

--- private_dot_mozilla/private_firefox/private_arkenfox.default/user-overrides.js
+++ private_dot_mozilla/private_firefox/private_arkenfox.default/user-overrides.js
@@ -1,3 +1,6 @@
+// Personal preferences
+user_pref("network.http.http3.enabled", true);
+
 /****************************************************************************
  * Arkenfox                                                                 *
  * version: 135                                                             *
