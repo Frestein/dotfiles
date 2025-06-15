7bc0565db3aff6f4344e71d9689e9fa3f4ddf28c
Frestein <frestein@tuta.io>
2025-06-15 15:45:54 +0500

    feat(hyprland): refactor kill active, use qalc instead of bc

--- dot_config/hypr/scripts/executable_killactive.sh
+++ dot_config/hypr/scripts/executable_killactive.sh
@@ -3,14 +3,12 @@
 LOCK_FILE="/tmp/killactive.lock"
 TIMEOUT=300
 
-if [ -f "$LOCK_FILE" ]; then
-    rm -f "$LOCK_FILE"
-    hyprctl dispatch killactive
-    exit 0
-fi
-
-touch "$LOCK_FILE"
 (
-    sleep "$(echo "$TIMEOUT / 1000" | bc -l)"
-    rm -f "$LOCK_FILE"
-) &
+    flock -n 9 || {
+        hyprctl dispatch killactive
+        exit 0
+    }
+
+    sleep $((TIMEOUT / 1000)).$((TIMEOUT % 1000))
+
+) 9>"$LOCK_FILE"
