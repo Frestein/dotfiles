af8f82563dc9f5ecb883d0d38615de3c615c7c1b
Frestein <frestein@tuta.io>
2025-07-09 04:02:59 +0500

    fix(scripts): facepalm

--- dot_config/hypr/scripts/fuzzel/executable_asroot.sh
+++ dot_config/hypr/scripts/fuzzel/executable_asroot.sh
@@ -5,27 +5,27 @@ hypr="$XDG_CONFIG_HOME/hypr"
 run_command() {
     case "$1" in
     "Foot")
-        "$hypr/scripts/asroot.sh" foot -e
+        "$hypr/scripts/asroot.sh" foot
         ;;
     "Zapret")
-        "$hypr/scripts/asroot.sh" foot -e sh -c "cd /opt/zapret && exec fish"
+        "$hypr/scripts/asroot.sh" foot --working-directory /opt/zapret
         ;;
     "DNSCrypt")
-        "$hypr/scripts/asroot.sh" foot -e sh -c "cd /etc/dnscrypt-proxy && exec fish"
+        "$hypr/scripts/asroot.sh" foot --working-directory /etc/dnscrypt-proxy
         ;;
     "Etckeeper")
-        "$hypr/scripts/asroot.sh" foot -e sh -c "cd /etc && exec fish"
+        "$hypr/scripts/asroot.sh" foot --working-directory /etc
         ;;
     "Neovim")
-        "$hypr/scripts/asroot.sh" foot -e nvim
+        "$hypr/scripts/asroot.sh" foot nvim
         ;;
     "Yazi")
-        "$hypr/scripts/asroot.sh" foot -e yazi
+        "$hypr/scripts/asroot.sh" foot yazi
         ;;
     esac
 }
 
-options=" Foot\n󰑩 Zapret\n󰒒 DNSCrypt\n Etckeeper\n Neovim\n󰇥 Yazi"
+options=" Foot\n Neovim\n󰇥 Yazi\n󰑩 Zapret\n󰒒 DNSCrypt\n Etckeeper"
 selected_option=$(echo "$options" | fuzzel -d \
     -l 6 \
     -p " " \
