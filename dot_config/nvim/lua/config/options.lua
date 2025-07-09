9635c42fabed22d53424ae2aa279e78d916a2f5a
Frestein <frestein@tuta.io>
2025-07-09 22:40:51 +0500

    feat(nvim/lang.toml): add tombi/taplo support

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -9,6 +9,7 @@ g.loaded_ruby_provider = 0
 
 --- LazyVim  ---
 g.lazyvim_python_lsp = "basedpyright"
+g.lazyvim_toml_lsp = "tombi"
 
 --- Neovide ---
 g.neovide_cursor_animation_length = 0
--- dot_config/nvim/lua/plugins/extras/lang/toml.lua
+++ dot_config/nvim/lua/plugins/extras/lang/toml.lua
@@ -1,18 +1,32 @@
+local lsp = vim.g.lazyvim_toml_lsp or "taplo"
+
 return {
 	recommended = function()
 		return LazyVim.extras.wants {
 			ft = "toml",
+			root = "*.toml",
 		}
 	end,
 
-	"stevearc/conform.nvim",
-	optional = true,
-	cmd = "ConformInfo",
-	---@module "conform"
-	---@type conform.setupOpts
-	opts = {
-		formatters_by_ft = {
-			toml = { "taplo" },
+	{
+		"neovim/nvim-lspconfig",
+		optional = true,
+		setup = function()
+			vim.lsp.enable(lsp)
+		end,
+	},
+
+	{
+
+		"stevearc/conform.nvim",
+		optional = true,
+		cmd = "ConformInfo",
+		---@module "conform"
+		---@type conform.setupOpts
+		opts = {
+			formatters_by_ft = {
+				toml = { lsp },
+			},
 		},
 	},
 }
