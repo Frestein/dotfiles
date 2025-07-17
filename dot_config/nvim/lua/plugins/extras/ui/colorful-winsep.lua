b06b18004679e534d1788d01c88d373ce54e6983
Frestein <frestein@tuta.io>
2025-07-17 20:40:49 +0500

    refactor(nvim): shit

--- dot_config/nvim/lua/plugins/extras/ui/colorful-winsep.lua
+++ dot_config/nvim/lua/plugins/extras/ui/colorful-winsep.lua
@@ -1,11 +1,9 @@
-local opts = {
-	hi = {
-		fg = vim.api.nvim_get_hl(0, { name = "NvimSeparator" }).fg,
-	},
-}
-
 return {
 	"nvim-zh/colorful-winsep.nvim",
 	event = "WinLeave",
-	opts = opts,
+	opts = {
+		hi = {
+			fg = vim.api.nvim_get_hl(0, { name = "NvimSeparator" }).fg,
+		},
+	},
 }
