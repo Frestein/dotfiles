fba7ba38d69469d9354f3978e50a68f37b24bcf8
Frestein <frestein@tuta.io>
2025-07-17 20:57:35 +0500

    feat(nvim/editor.import): new extra

---
+++ dot_config/nvim/lua/plugins/extras/editor/import.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"piersolenski/import.nvim",
+		dependencies = "folke/snacks.nvim",
+		opts = {
+			picker = "snacks",
+		},
+		keys = {
+			{
+				"<leader>ci",
+				function()
+					require("import").pick()
+				end,
+				desc = "Import",
+			},
+		},
+	},
+}
