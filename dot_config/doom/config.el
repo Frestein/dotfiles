d003c46a802d5f3a241739e19d118e35b11716c4
Frestein <frestein@tuta.io>
2025-08-01 04:39:14 +0500

    feat(doom): change fonts, refactor

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -18,36 +18,25 @@
 ;; Theme
 (setq doom-theme 'doom-gruvbox)
 
-;; Org
-(setq org-directory "~/Documents/org/")
-
 ;; Global Auto Revert
 ;; A buffer can get out of sync with respect to its visited file on disk if that file is changed by another program. To keep it up to date, you can enable Auto Revert mode by typing M-x auto-revert-mode, or you can set it to be turned on globally with ‘global-auto-revert-mode’.  I have also turned on Global Auto Revert on non-file buffers, which is especially useful for ‘dired’ buffers.
-(global-auto-revert-mode)
+(global-auto-revert-mode t)
 (setq global-auto-revert-non-file-buffers t)
 
-;; Line numbers
+;; Line settings
 (setq display-line-numbers-type 'relative)
+(visual-line-mode t)
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
+;; Fonts
+(setq doom-font (font-spec :family "Maple Mono NF" :size 14)
+      doom-variable-pitch-font (font-spec :family "Source Code Pro" :size 14)
+      doom-big-font (font-spec :family "Maple Mono NF" :size 24)
+      doom-symbol-font (font-spec :family "JoyPixels" :size 14)
+      doom-serif-font (font-spec :family "Noto Serif" :size 14))
+
+(custom-set-faces!
+  '(font-lock-comment-face :slant italic)
+  '(font-lock-keyword-face :slant italic))
 
 ;; Shell
 (setq shell-file-name (executable-find "bash"))
@@ -60,14 +49,25 @@
 (remove-hook 'doom-first-file-hook #'ultra-scroll-mode)
 
 ;; Mappings
-(map! (:leader
-       (:prefix ("A" . "app")
-                (:when (modulep! :app rss)
-                  :desc "Elfeed" "e" #'elfeed))))
+(map! :leader
+      (:prefix ("A" . "app")
+               (:when (modulep! :app rss)
+                 :desc "Elfeed"
+                 "e" #'elfeed)))
 
 ;; Git
 ;; Magit
-(after! magit
+(when (modulep! :tools magit)
   (setq magit-repository-directories
         '(("~/Projects/" . 2)
           ("~/.local/share/chezmoi" . 1))))
+
+;; Org
+(after! org
+  (setq org-directory "~/Documents/org/"
+        org-hide-emphasis-markers t))
+
+;; Zen
+(when (modulep! :ui zen)
+  (setq +zen-text-scale 0
+        writeroom-width 100))
