8b9aaaa6bf9571795d1acb4e7472c9ef22510a7c
Frestein <frestein@tuta.io>
2025-08-04 19:38:08 +0500

    feat(nvim/langmapper): add flash.nvim and snacks.nvim support

--- dot_config/nvim/lua/plugins/extras/utils/langmapper.lua
+++ dot_config/nvim/lua/plugins/extras/utils/langmapper.lua
@@ -48,4 +48,52 @@ return {
 			end
 		end,
 	},
+
+	{
+		"folke/snacks.nvim",
+		optional = true,
+		opts = function()
+			local lmu = require "langmapper.utils"
+			local sn_util = Snacks.util
+			local normkey_orig = sn_util.normkey
+
+			sn_util.normkey = function(key)
+				if key then
+					key = lmu.translate_keycode(key, "default", "ru")
+				end
+
+				return normkey_orig(key)
+			end
+		end,
+	},
+
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		config = function(_, opts)
+			local flash = require "flash"
+			local langmapper = require "langmapper"
+
+			for _, mode in pairs { "n", "x", "o" } do
+				langmapper.original_set_keymap(mode, "s", "", {
+					nowait = true,
+					desc = "Flash",
+					callback = function()
+						flash.jump()
+					end,
+				})
+				langmapper.original_set_keymap(mode, "ы", "", {
+					nowait = true,
+					desc = "Flash",
+					callback = function()
+						flash.jump {
+							labels = "олджавыфгнрткепимйцуячсшщзьбюАВЫФОЛДЖЙЦУКЕНГШЩЗ",
+						}
+					end,
+				})
+			end
+
+			flash.setup(opts)
+		end,
+	},
 }
