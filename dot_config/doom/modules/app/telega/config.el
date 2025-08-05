b5b1024e7f08026205010b36d575c710a564bec7
Frestein <frestein@tuta.io>
2025-08-05 19:33:48 +0500

    refactor(doom/app.telega): 4 8 15 16 23 42

--- dot_config/doom/modules/app/telega/config.el
+++ dot_config/doom/modules/app/telega/config.el
@@ -1,19 +1,17 @@
 ;;; app/telega/config.el -*- lexical-binding: t; -*-
 
-(map! (:leader
-       (:prefix ("A" . "app")
-         :desc "Telega" "t" telega-prefix-map)))
-
-(use-package!
-  telega
+(use-package! telega
   :bind-keymap ("C-c t" . telega-prefix-map)
   :hook (telega-load . telega-appindicator-mode)
   :config
   (setq telega-server-libs-prefix "/usr")
   (setq telega-translate-to-language-by-default "ru"))
 
-(use-package!
-  telega-mnz
+(map! (:leader
+       (:prefix ("A" . "app")
+        :desc "Telega" "t" telega-prefix-map)))
+
+(use-package! telega-mnz
   :when (modulep! +mnz)
   :after-call telega
   :custom
@@ -22,21 +20,21 @@
 
 (use-package! language-detection
   :when (modulep! +mnz)
-  ;; NOTE the `language-detection-string' function is already autoloaded,
+  ;; NOTE: The `language-detection-string' function is already autoloaded,
   ;; so we can safely defer it.
   :defer t)
 
-;; (use-package
-;;   telega-dashboard
-;;   :ensure nil
-;;   :after telega
+;; TODO: WIP
+;; (use-package telega-dashboard
+;; :when (modulep! +dashboard)
+;;   :after-call telega
 ;;   :config (add-to-list 'dashboard-items '(telega-chats . 5)))
 
-(use-package!
-  telega-stories
+(use-package! telega-stories
   :when (modulep! +stories)
   :after-call telega
-  :bind (:map telega-root-mode-map ("v e" . telega-view-emacs-stories))
+  :bind (:map telega-root-mode-map
+              ("v e" . telega-view-emacs-stories))
   :config (telega-stories-mode 1))
 
 (use-package! telega-dired-dwim :after-call telega)
