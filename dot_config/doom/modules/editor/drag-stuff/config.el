7ea28134ad7222a55263149cc9f6fc4f2093918e
Frestein <frestein@tuta.io>
2025-08-05 18:45:21 +0500

    feat(doom/editor.drag-stuff): new module

---
+++ dot_config/doom/modules/editor/drag-stuff/config.el
@@ -0,0 +1,12 @@
+;;; editor/drag-stuff/config.el -*- lexical-binding: t; -*-
+
+(use-package! drag-stuff
+  :hook ((prog-mode org-mode text-mode) . drag-stuff-mode)
+  :config
+  (map! :map drag-stuff-mode-map
+        :n "M-h" #'drag-stuff-left
+        :n "M-j" #'drag-stuff-down
+        :n "M-k" #'drag-stuff-up
+        :n "M-l" #'drag-stuff-right
+        :v "M-j" #'drag-stuff-down
+        :v "M-k" #'drag-stuff-up))
---
+++ dot_config/doom/modules/editor/drag-stuff/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; editor/drag-stuff/packages.el
+
+(package! drag-stuff)
