037fb58eb290449535ffe87ecf1d88e090c477df
Frestein <frestein@tuta.io>
2025-08-21 20:04:39 +0500

    chore(doom/tools.screenshot): add config

--- dot_config/doom/modules/tools/screenshot/config.el
+++ dot_config/doom/modules/tools/screenshot/config.el
@@ -3,3 +3,20 @@
 (map! :leader
       (:prefix-map ("c" . "code")
        :desc "Screenshot Code" "S" #'screenshot))
+
+(use-package! screenshot
+  :hook (screenshot-buffer-creation-hook . g-screenshot-on-buffer-creation)
+  :config
+  (setq screenshot-line-numbers-p t
+        screenshot-min-width 80
+        screenshot-max-width 300
+        screenshot-truncate-lines-p nil
+        screenshot-text-only-p nil
+        screenshot-font-family "Maple Mono NF"
+        screenshot-font-size 10
+        screenshot-border-width 16
+        screenshot-radius 10
+        screenshot-shadow-intensity 90
+        screenshot-shadow-radius 16
+        screenshot-shadow-offset-horizontal 8
+        screenshot-shadow-offset-vertical 8))
