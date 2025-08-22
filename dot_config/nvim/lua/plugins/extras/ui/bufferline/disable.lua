a60e8122c7eee1bac13376f0286765e564fe2526
Frestein <frestein@tuta.io>
2025-08-22 11:04:24 +0500

    feat(nvim/ui.bufferline): show tabs in lualine
    - Show tabs in `lualine_z` section if bufferline disabled.
    - Disable tabline.

--- dot_config/nvim/lua/plugins/extras/ui/bufferline/disable.lua
+++ dot_config/nvim/lua/plugins/extras/ui/bufferline/disable.lua
@@ -1,3 +1,17 @@
+vim.o.showtabline = 0
+
 return {
 	{ "akinsho/bufferline.nvim", enabled = false, optional = true },
+
+	{
+		"nvim-lualine/lualine.nvim",
+		optional = true,
+		opts = {
+			sections = {
+				lualine_z = {
+					"tabs",
+				},
+			},
+		},
+	},
 }
