829e294f83b193b839b374507d7e238927451434
Frestein <frestein@tuta.io>
2025-09-05 23:57:05 +0500

    fix(doom/jinx): correct mode name

--- dot_config/doom/modules/checkers/jinx/config.el
+++ dot_config/doom/modules/checkers/jinx/config.el
@@ -1,12 +1,12 @@
 ;;; checkers/jinx/config.el -*- lexical-binding: t; -*-
 
 (use-package! jinx
-  :hook ((prog-mode conf-mode org-mode text-mode) . drag-stuff-mode)
+  :hook ((prog-mode conf-mode org-mode text-mode) . jinx-mode)
   :bind (("M-$" . jinx-correct)
          ("C-M-$" . jinx-languages))
   :config
   (setq jinx-languages "ru_RU en_US"
-        jinx-delay 1.0)
+        jinx-delay 0.2)
 
   (after! vertico-multiform
     (add-to-list 'vertico-multiform-categories
