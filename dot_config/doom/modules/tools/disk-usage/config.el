cf00d13c6722bbfe97e68b7c1338e6d1d66a80e8
Frestein <frestein@tuta.io>
2025-09-16 23:59:23 +0500

    feat(doom/disk-usage): exclude dirs

--- dot_config/doom/modules/tools/disk-usage/config.el
+++ dot_config/doom/modules/tools/disk-usage/config.el
@@ -1,4 +1,21 @@
 ;;; tools/disk-usage/config.el -*- lexical-binding: t; -*-
 
 (use-package! disk-usage
-  :hook (disk-usage-mode . display-line-numbers-mode))
+  :hook (disk-usage-mode . display-line-numbers-mode)
+  :custom
+  (disk-usage-du-args
+   "-sb --exclude=.git --exclude=GDrive")
+  (disk-usage--du-args
+   "-sb --exclude=.git --exclude=GDrive")
+  :config
+  (defvar disk-usage-exclude-dirs
+    '(".git" "GDrive")
+    "List of directory names to exclude from disk-usage calculations.")
+
+  (defun disk-usage-filter-exclude (path _attributes)
+    "Exclude PATH if its final directory name is in `disk-usage-exclude-dirs`."
+    (not (member (file-name-nondirectory (directory-file-name path))
+                 disk-usage-exclude-dirs)))
+
+  (add-to-list 'disk-usage-available-filters 'disk-usage-filter-exclude)
+  (add-to-list 'disk-usage-default-filters 'disk-usage-filter-exclude))
