6cdf10c76f3986882375d8abcffa71e76bb01591
Frestein <frestein@tuta.io>
2025-09-24 15:30:27 +0500

    refactor(doom/forgejo): move personal config to DOOMDIR

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -64,6 +64,19 @@
       (add-hook! server-after-make-frame (telega 'no-popup))
     (add-hook! doom-after-init (telega 'no-popup))))
 
+;; Forgejo
+(when (modulep! :tools forgejo)
+  (setq fj-host "https://codeberg.org"
+        fj-user "Frestein"
+        fj-token-use-auth-source nil)
+
+  (defun frestein/fj-set-token ()
+    (setq fj-token (auth-source-pass-get 'secret "work/git/codeberg.org/api/fj.el")))
+
+  (if (daemonp)
+      (add-hook! server-after-make-frame (frestein/fj-set-token))
+    (add-hook! doom-init-ui (frestein/fj-set-token))))
+
 ;; Smooth scroll
 (when (modulep! :ui smooth-scroll)
   ;; Disable ultra-scroll
--- dot_config/doom/modules/tools/forgejo/config.el
+++ dot_config/doom/modules/tools/forgejo/config.el
@@ -1,14 +1,3 @@
 ;;; app/forgejo/config.el -*- lexical-binding: t; -*-
 
-(use-package! fj
-  :config
-  (setq fj-host "https://codeberg.org"
-        fj-user "Frestein"
-        fj-token-use-auth-source nil)
-
-  (defun frestein/fj-set-token ()
-    (setq fj-token (auth-source-pass-get 'secret "work/git/codeberg.org/api/fj.el")))
-
-  (if (daemonp)
-      (add-hook! server-after-make-frame (frestein/fj-set-token))
-    (add-hook! doom-init-ui (frestein/fj-set-token))))
+(use-package! fj)
