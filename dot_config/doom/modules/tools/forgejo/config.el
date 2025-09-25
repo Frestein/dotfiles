ea5c12bcd518b7b4ed8803fe291e95bacd1c1266
Frestein <frestein@tuta.io>
2025-09-26 01:12:35 +0500

    feat(doom/fj): censor fj-token

--- dot_config/doom/modules/tools/forgejo/config.el
+++ dot_config/doom/modules/tools/forgejo/config.el
@@ -1,3 +1,11 @@
 ;;; app/forgejo/config.el -*- lexical-binding: t; -*-
 
-(use-package! fj)
+(use-package! fj
+  :config
+  (when (modulep! :completion vertico)
+    (after! marginalia
+      (setq marginalia-censor-variables
+            (list
+             (concat
+              (car marginalia-censor-variables)
+              "\\|^fj-token$"))))))
