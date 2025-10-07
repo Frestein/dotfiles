e8288a08dc28c47bea708f21a5ff0529b6d5e95c
Frestein <frestein@tuta.io>
2025-10-07 19:00:31 +0500

    chore(fish): bump omp setup

--- dot_config/private_fish/conf.d/01-omp.fish
+++ dot_config/private_fish/conf.d/01-omp.fish
@@ -7,14 +7,9 @@ oh-my-posh init fish --config="$XDG_CONFIG_HOME/oh-my-posh/themes/bubblesline-fi
 function rerender_on_bind_mode_change --on-variable fish_bind_mode
     if test "$fish_bind_mode" != paste -a "$fish_bind_mode" != "$FISH__BIND_MODE"
         set -gx FISH__BIND_MODE $fish_bind_mode
-        omp_repaint_prompt
     end
 end
 
+# INFO: This function is masked and does nothing
 function fish_default_mode_prompt --description "Display vi prompt mode"
-    # This function is masked and does nothing
-end
-
-function rerender_on_dir_change --on-variable PWD
-    omp_repaint_prompt
 end
