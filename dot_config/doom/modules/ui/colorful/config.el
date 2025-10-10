ee438e8603cc03e614b3bfe52d40ac2a1f87f97e
Frestein <frestein@tuta.io>
2025-10-10 14:29:47 +0500

    feat(doom/colorful): support helpful and conf mode

--- dot_config/doom/modules/ui/colorful/config.el
+++ dot_config/doom/modules/ui/colorful/config.el
@@ -4,6 +4,10 @@
   :hook (doom-after-init . global-colorful-mode)
   :config
   (add-to-list 'global-colorful-modes 'conf-mode)
+  (add-to-list 'colorful-extra-color-keyword-functions
+               '(conf-mode . colorful-add-color-names))
   (when (modulep! :lang emacs-lisp)
-    (add-to-list 'global-colorful-modes 'helpful-mode))
+    (add-to-list 'global-colorful-modes 'helpful-mode)
+    (add-to-list 'colorful-extra-color-keyword-functions
+                 '(helpful-mode . colorful-add-color-names)))
   :custom-face (colorful-base ((nil (:box nil)))))
