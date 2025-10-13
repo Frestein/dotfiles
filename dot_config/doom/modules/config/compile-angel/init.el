eefee1dfb64b46e1b596537d882548b0ca90ad78
Frestein <frestein@tuta.io>
2025-10-14 03:31:38 +0500

    refactor(compile-angel): clean up

--- dot_config/doom/modules/config/compile-angel/init.el
+++ dot_config/doom/modules/config/compile-angel/init.el
@@ -1,14 +1,4 @@
 ;;; config/compile-angel/init.el -*- lexical-binding: t; -*-
 
 (use-package! compile-angel
-  :config
-  ;; Set `compile-angel-verbose' to nil to disable compile-angel messages.
-  ;; (When set to nil, compile-angel won't show which file is being compiled.)
-  (setq compile-angel-verbose nil)
-
-  ;; Uncomment the line below to compile automatically when an Elisp file is saved
-  ;; (add-hook 'emacs-lisp-mode-hook #'compile-angel-on-save-local-mode)
-
-  ;; A global mode that compiles .el files before they are loaded
-  ;; using `load' or `require'.
-  (compile-angel-on-load-mode t))
+  :hook (doom-after-modules-config . compile-angel-on-load-mode))
