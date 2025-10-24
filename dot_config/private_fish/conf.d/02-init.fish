67560b72c608154c4499d8c86d39f5ccf6f1012e
Frestein <frestein@tuta.io>
2025-10-25 01:18:23 +0500

    fix(fish): workaround atuin fish issue
    - atuinsh/atuin#2803
    - atuinsh/atuin#2940

--- dot_config/private_fish/conf.d/02-init.fish
+++ dot_config/private_fish/conf.d/02-init.fish
@@ -5,19 +5,8 @@ end
 zoxide init fish | source
 
 # FIX: fish 4.0 deprecates `bind -k`. transform's Atuin's init to drop -k and ensure up-binding works
-if type -q atuin
-    set -l __atuin_init (atuin init fish | string replace -ra -- 'bind -M ([^ ]+)\s+-k ' 'bind -M $1 ' | string replace -ra -- 'bind\s+-k ' 'bind ')
-    if test -n "$__atuin_init"
-        printf '%s\n' $__atuin_init | source
-        if functions -q _atuin_bind_up
-            bind up _atuin_bind_up
-            bind -M insert up _atuin_bind_up
-        end
-    else
-        # fallback: source unmodified but silence deprecation noise
-        atuin init fish 2>/dev/null | source
-    end
-end
+# https://github.com/atuinsh/atuin/issues/2803, https://github.com/atuinsh/atuin/issues/2940
+atuin init fish | sed "s/-k up/up/g" | source
 
 # fzf --fish | source
 tv init fish | source
