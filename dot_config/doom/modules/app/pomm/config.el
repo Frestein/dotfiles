c323d736d6203644bf9367bfe2981cac3ec9707f
Frestein <frestein@tuta.io>
2025-11-14 23:17:47 +0500

    feat(doom/pomm): audio playback

--- dot_config/doom/modules/app/pomm/config.el
+++ dot_config/doom/modules/app/pomm/config.el
@@ -3,4 +3,24 @@
 (use-package pomm
   :hook (doom-after-init . pomm-mode-line-mode)
   :config
-  (setq alert-default-style 'libnotify))
+  (setq alert-default-style 'libnotify
+        pomm-audio-enabled t
+        pomm-audio-player-executable (executable-find "mpv"))
+
+  ;; INFO: correct mpv
+  (defun pomm--maybe-play-sound (kind)
+    (when pomm-audio-enabled
+      (unless pomm-audio-player-executable
+        (error "No audio player executable! Set 'pomm-audio-player-executable'")
+        (setq pomm-audio-enabled nil))
+      (when-let ((play-sound (or (not (eq 'tick kind)) pomm-audio-tick-enabled))
+                 (sound (alist-get kind pomm-audio-files)))
+        (let* ((cmd (split-string pomm-audio-player-executable))
+               (prog (car cmd))
+               (args (cdr cmd)))
+          (when (string-match-p "mpv\\'" prog)
+            (setq args (append '("--no-terminal" "--no-config" "--volume=35" "--speed=1.2") args)))
+          (apply #'start-process
+                 "pomm-audio-player" nil
+                 prog
+                 (append args (list sound))))))))
