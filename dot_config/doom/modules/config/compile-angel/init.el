09cb986458c3a9b2b5aedcd98b50b364be27dddf
Frestein <frestein@tuta.io>
2025-11-20 12:59:14 +0500

    feat(doom): i need more native compile
    more more more more more more more

--- dot_config/doom/modules/config/compile-angel/init.el
+++ dot_config/doom/modules/config/compile-angel/init.el
@@ -1,4 +1,14 @@
 ;;; config/compile-angel/init.el -*- lexical-binding: t; -*-
 
+;; Ensure Emacs loads the most recent byte-compiled files.
+(setq load-prefer-newer t)
+
+;; Ensure that quitting only occurs once Emacs finishes native compiling,
+;; preventing incomplete or leftover compilation files in `/tmp`.
+(setq native-comp-async-query-on-exit t)
+
+;; Non-nil means to native compile packages as part of their installation.
+(setq package-native-compile t)
+
 (use-package! compile-angel
   :hook (doom-after-modules-config . compile-angel-on-load-mode))
