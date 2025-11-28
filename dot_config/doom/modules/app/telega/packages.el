fb26e7d56997b82c5d55a0f10d20f7cf5a40b64a
Frestein <frestein@tuta.io>
2025-11-28 17:42:26 +0500

    chore(doom/telega): pin 1.8.55 tdlib

--- dot_config/doom/modules/app/telega/packages.el
+++ dot_config/doom/modules/app/telega/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/telega/packages.el
 
-(package! telega)
+(package! telega :pin "c8000bb5a9549297d51bf95f24056b44990d1ecc")
 
 (when (modulep! +mnz)
   (package! language-detection))
