980f9f8b0f97496be758ad0061353c4f8ba7e12a
Frestein <frestein@tuta.io>
2025-12-01 17:41:39 +0500

    feat(doom): GTD and refactor Org

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -2,13 +2,18 @@
 ;;; $DOOMDIR/packages.el
 
 (when (modulep! :lang org)
-  (package! org-super-agenda)
   (package! org-expose-emphasis-markers)
-  ;; TODO: Adjust when this is added to GNU ELPA
-  (package! org-contacts
-    :recipe (:host github :repo "emacsmirror/org-contacts"))
+
   (package! corg
-    :recipe (:host github :repo "isamert/corg.el")))
+    :recipe (:host github :repo "isamert/corg.el"))
+
+  ;; TODO: Adjust when this is added to GNU ELPA
+  (when (modulep! :lang org +contacts2)
+    (package! org-contacts
+      :recipe (:host github :repo "emacsmirror/org-contacts")))
+
+  (when (modulep! :lang org +super)
+    (package! org-super-agenda)))
 
 (when (modulep! :tools magit)
   (when (modulep! :ui hl-todo)
