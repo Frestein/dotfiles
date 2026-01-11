9eca476ee5d70cb7b3003e3eec157ebbfb92da4b
Frestein <frestein@tuta.io>
2026-01-11 14:42:03 +0500

    fix(doom/gac): improve getting relative name
    https://github.com/ryuslash/git-auto-commit-mode/pull/49

--- dot_config/doom/modules/tools/git-auto-commit/config.el
+++ dot_config/doom/modules/tools/git-auto-commit/config.el
@@ -5,6 +5,16 @@
   (gac-debounce-interval 600)
   (gac-automatically-push-p t)
   (gac-silent-message-p t)
-  (gac-default-message
-   (lambda (filename)
-     (concat "Update " (gac-relative-file-name filename)))))
+  :config
+  (setq gac-default-message
+        (lambda (filename)
+          (concat "Update " (gac-relative-file-name filename))))
+
+  ;; INFO: fix https://github.com/ryuslash/git-auto-commit-mode/pull/49
+  (defun gac-relative-file-name (filename)
+    "Find the path to FILENAME relative to its git directory."
+    (let* ((default-directory (file-name-directory (expand-file-name filename)))
+           (git-dir
+            (string-trim-right
+             (shell-command-to-string "git rev-parse --show-toplevel"))))
+      (file-relative-name filename git-dir))))
