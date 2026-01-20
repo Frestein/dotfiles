e9864dbd5ba4260ba1835842c26a05d39912d403
Frestein <frestein@tuta.io>
2026-01-20 18:18:41 +0500

    chore(nyxt): misc

--- dot_config/nyxt/config.lisp
+++ dot_config/nyxt/config.lisp
@@ -1,7 +1,9 @@
 ;; Keybindings
-(define-configuration input-buffer
-  ((default-modes
-    (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+(define-configuration (input-buffer)
+    ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+
+(define-configuration (prompt-buffer)
+    ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
 
 ;; Adblocking
 ;; FIX: Broken on electron backend
