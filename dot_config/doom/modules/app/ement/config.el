f9d8635b2f97b060184178cfba4c5aa962a0ac92
Frestein <frestein@tuta.io>
2026-01-27 06:32:21 +0500

    feat(doom/ement): basic config

--- dot_config/doom/modules/app/ement/config.el
+++ dot_config/doom/modules/app/ement/config.el
@@ -1,1 +1,8 @@
 ;;; app/ement/config.el -*- lexical-binding: t; -*-
+
+(use-package! ement
+  :commands ement-connect
+  :config
+  (setq! ement-sessions-file (concat doom-cache-dir "ement.el"))
+  (setq! ement-save-sessions t)
+  (setq! ement-room-send-message-filter #'ement-room-send-org-filter))
