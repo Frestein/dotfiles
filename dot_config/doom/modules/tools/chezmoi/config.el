82202c1084a3344510b535d664c360133cb09444
Frestein <frestein@tuta.io>
2026-02-04 08:57:50 +0500

    feat(doom/chezmoi): add `chezmoi-status` and use popups instead of full buffers

--- dot_config/doom/modules/tools/chezmoi/config.el
+++ dot_config/doom/modules/tools/chezmoi/config.el
@@ -18,22 +18,47 @@
     :type '(string)
     :group 'chezmoi)
 
-  (defun fr/chezmoi-diff (arg)
+  (defun chezmoi-status (arg)
+    "View output of =chezmoi status= in a status-buffer."
+    (interactive "i")
+    (let ((b (get-buffer-create "*chezmoi-status*")))
+      (with-current-buffer b
+        (let ((inhibit-read-only t))
+          (erase-buffer)
+          (chezmoi--locally
+           (shell-command (concat chezmoi-command " status --use-builtin-diff ") b))))
+      (unless arg
+        (let ((window (display-buffer b
+                                      '((display-buffer-at-bottom)
+                                        (window-height . 0.25)))))
+          (select-window window)
+          (with-current-buffer b
+            (diff-mode)
+            (read-only-mode 1)
+            (whitespace-mode 0))))
+      b))
+
+  (defun chezmoi-diff (arg)
     "View output of =chezmoi diff= in a diff-buffer.
 If ARG is non-nil, switch to the diff-buffer."
     (interactive "i")
     (let ((b (get-buffer-create "*chezmoi-diff*")))
       (with-current-buffer b
-        (erase-buffer)
-        (chezmoi--locally (shell-command (concat chezmoi-command " diff --use-builtin-diff --pager " (shell-quote-argument chezmoi-pager-command)) b)))
+        (let ((inhibit-read-only t))
+          (erase-buffer)
+          (chezmoi--locally
+           (shell-command (concat chezmoi-command " diff --use-builtin-diff "
+                                  "--pager " (shell-quote-argument chezmoi-pager-command)) b))))
       (unless arg
-        (switch-to-buffer b)
-        (diff-mode)
-        (whitespace-mode 0))
+        (let ((window (display-buffer b
+                                      '((display-buffer-pop-up-window)
+                                        (window-height . 0.50)))))
+          (select-window window)
+          (with-current-buffer b
+            (diff-mode)
+            (read-only-mode 1)
+            (whitespace-mode 0))))
       b))
-
-  (advice-add #'chezmoi-diff
-              :override #'fr/chezmoi-diff)
 
   (when (modulep! :editor evil)
     (add-hook 'chezmoi-mode-hook #'+chezmoi--evil-h)))
