a25c1effa6e862c0a252fe8016b71b1cfbc60f60
Frestein <frestein@tuta.io>
2026-02-06 11:31:09 +0500

    chore(fish): remove old workaround

--- dot_config/private_fish/conf.d/20-foot.fish
+++ dot_config/private_fish/conf.d/20-foot.fish
@@ -2,13 +2,6 @@ if not status is-interactive && test "$CI" != true
     exit
 end
 
-# Jumping between prompts
-# No configuration is required for fish 4.0+
-# Remove this in fish 4.0+
-function mark_prompt_start --on-event fish_prompt
-    echo -en "\e]133;A\e\\"
-end
-
 # Piping last command's output
 function foot_cmd_start --on-event fish_preexec
     echo -en "\e]133;C\e\\"
