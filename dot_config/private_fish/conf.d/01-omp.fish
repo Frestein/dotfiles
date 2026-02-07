28710e676ac27e8fd524458ffc9e4ed03b5e11d9
Frestein <frestein@tuta.io>
2026-02-07 11:13:52 +0500

    fix(fish): correct prompt repaint on vi mode change

--- dot_config/private_fish/conf.d/01-omp.fish
+++ dot_config/private_fish/conf.d/01-omp.fish
@@ -4,9 +4,10 @@ end
 
 oh-my-posh init fish --config="$XDG_CONFIG_HOME/oh-my-posh/themes/bubblesline-fish.omp.toml" | source
 
-function rerender_on_bind_mode_change --on-variable fish_bind_mode
+function rerender_on_bind_mode_change --on-variable fish_bind_mode -d "Trigger oh-my-posh repaint on vi mode change"
     if test "$fish_bind_mode" != paste -a "$fish_bind_mode" != "$FISH__BIND_MODE"
         set -gx FISH__BIND_MODE $fish_bind_mode
+        omp_repaint_prompt
     end
 end
 
