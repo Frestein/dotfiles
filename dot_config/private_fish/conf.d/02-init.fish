8a60b551a767f4ddc64628034f78503237a62612
Frestein <frestein@tuta.io>
2026-02-12 12:24:34 +0500

    refactor(fish): remove workaround for atuin + fish issue
    - https://github.com/atuinsh/atuin/issues/2803
    - https://github.com/atuinsh/atuin/issues/2940

--- dot_config/private_fish/conf.d/02-init.fish
+++ dot_config/private_fish/conf.d/02-init.fish
@@ -3,12 +3,7 @@ if not status is-interactive && test "$CI" != true
 end
 
 zoxide init fish | source
-
-# FIX: fish 4.0 deprecates `bind -k`. transform's Atuin's init to drop -k and ensure up-binding works
-# https://github.com/atuinsh/atuin/issues/2803, https://github.com/atuinsh/atuin/issues/2940
-atuin init fish | sed "s/-k up/up/g" | source
-
-# fzf --fish | source
+atuin init fish | source
 tv init fish | source
 batpipe | source
 direnv hook fish | source
