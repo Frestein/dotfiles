f3225d4c0db73d85b9456fab08a13653d0accba3
Frestein <frestein@tuta.io>
2026-03-02 16:39:02 +0500

    feat(hyprland): correct UX for specials

--- dot_config/hypr/scripts/executable_close_special.sh
+++ dot_config/hypr/scripts/executable_close_special.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-active=$(hyprctl -j monitors | jq --raw-output '.[] | select(.focused==true).specialWorkspace.name | split(":") | if length > 1 then .[1] else "" end')
+active=$(hyprctl activewindow -j | jq -r '.workspace.name | split(":") | if length > 1 then .[1] else "" end')
 
 if [[ ${#active} -gt 0 ]]; then
     hyprctl dispatch togglespecialworkspace "$active"
+    exit 0
 fi
+
+exit 1
