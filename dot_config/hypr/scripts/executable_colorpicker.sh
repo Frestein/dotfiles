77e80703aa2c5c599b3485ce86f111d5dd59ca70
Frestein <frestein@tuta.io>
2026-03-05 04:30:46 +0500

    refactor(hyprland): rewrite colorpicker logic, move to POSIX, clean up

--- dot_config/hypr/scripts/executable_colorpicker.sh
+++ dot_config/hypr/scripts/executable_colorpicker.sh
@@ -1,13 +1,21 @@
-#!/usr/bin/env dash
+#!/usr/bin/env sh
 
-color=$(hyprpicker)
-image="/tmp/$color.png"
+if ! command -v hyprpicker >/dev/null 2>&1; then
+    notify-send -u critical -h string:x-dunst-stack-tag:colorpicker "Color Picker" "hyprpicker not found"
+    exit 1
+fi
+
+color=$(hyprpicker -a)
 
 if [ "$color" ]; then
-    # Copy color code to clipboard
-    echo "$color" | tr -d "\n" | wl-copy
+    image=$(mktemp -t colorpicker-XXXXXX.png)
+
     # Generate preview
     magick -size 48x48 xc:"$color" "$image"
+
     # Send notification with color
     notify-send -u low -h string:x-dunst-stack-tag:colorpicker -i "$image" "$color, copied to clipboard."
+
+    # Remove temporary preview
+    (sleep 1 && rm -f "$image") &
 fi
