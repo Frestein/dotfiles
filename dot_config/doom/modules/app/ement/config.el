1a0565ef3793f40abf97349f14fd138a140bce00
Frestein <frestein@tuta.io>
2026-03-07 19:12:56 +0500

    chore(ement): disable dbus notify

--- dot_config/doom/modules/app/ement/config.el
+++ dot_config/doom/modules/app/ement/config.el
@@ -4,4 +4,5 @@
   :config
   (setopt ement-sessions-file (expand-file-name "ement.el" doom-cache-dir))
   (setopt ement-save-sessions t)
-  (setopt ement-room-send-message-filter #'ement-room-send-org-filter))
+  (setopt ement-room-send-message-filter #'ement-room-send-org-filter)
+  (setq ement-notify-dbus-p nil))
