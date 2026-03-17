d5f18510fb1f1eb1cbae39d11656737cea93eff9
Frestein <frestein@tuta.io>
2026-03-18 01:03:17 +0500

    refactor(scripts): POSIX, names

--- dot_config/hypr/scripts/autostart/executable_gsettings.sh
+++ dot_config/hypr/scripts/autostart/executable_gsettings.sh
@@ -1,22 +1,22 @@
-#!/usr/bin/env dash
+#!/usr/bin/env sh
 
-config="$XDG_CONFIG_HOME/gtk-3.0/settings.ini"
-gnome_schema="org.gnome.desktop.interface"
+GTK_CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/settings.ini"
+GNOME_SCHEMA="org.gnome.desktop.interface"
 
-[ ! -f "$config" ] && exit 1
+[ ! -f "$GTK_CONFIG" ] && exit 1
 
 command -v gsettings >/dev/null 2>&1 || exit 1
 
 get_key() {
     key="$1"
-    sed -n "s/^[[:space:]]*${key}[[:space:]]*=[[:space:]]*['\"]*\([^'\"]*\)['\"]*/\1/p" "$config" | head -n1
+    sed -n "s/^[[:space:]]*${key}[[:space:]]*=[[:space:]]*['\"]*\([^'\"]*\)['\"]*/\1/p" "$GTK_CONFIG" | head -n1
 }
 
-font_name=$(get_key "gtk-font-name")
-icon_theme=$(get_key "gtk-icon-theme-name")
+GTK_FONT_NAME=$(get_key "gtk-font-name")
+GTK_ICON_THEME=$(get_key "gtk-icon-theme-name")
 
-[ "$icon_theme" != "" ] && gsettings set "$gnome_schema" icon-theme "$icon_theme"
-[ "$font_name" != "" ] && gsettings set "$gnome_schema" font-name "$font_name"
+[ "$GTK_ICON_THEME" != "" ] && gsettings set "$GNOME_SCHEMA" icon-theme "$GTK_ICON_THEME"
+[ "$GTK_FONT_NAME" != "" ] && gsettings set "$GNOME_SCHEMA" font-name "$GTK_FONT_NAME"
 
 # Remove window buttons
 gsettings set org.gnome.desktop.wm.preferences button-layout ""
