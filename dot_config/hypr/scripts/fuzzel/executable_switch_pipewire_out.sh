078601c69f8e9e49b46fd9b3917b1eec0135fafa
Frestein <frestein@tuta.io>
2026-03-18 01:22:56 +0500

    refactor(scripts): unify, POSIX, checks, usage

--- dot_config/hypr/scripts/fuzzel/executable_switch_pipewire_out.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env dash
-
-pactl set-default-sink "$(pactl list short sinks |
-    awk '{print $2}' |
-    fuzzel -d \
-        -p 'Switch Output ' \
-        --placeholder 'Choose...' \
-        -w 50 \
-        --minimal-lines)"
