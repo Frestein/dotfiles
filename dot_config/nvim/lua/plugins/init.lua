e6ce77ccb9c135e7c71bddbdc94ee5995c20206a
Frestein <frestein@tuta.io>
2026-05-10 13:26:50 +0500

    fix(nvim): temporarily use master LazyVim
    For refactorig.nvim 2.0 to work correctly, asinc.nvim is required,
    a fix for which has not yet been released.

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,3 +1,7 @@
 return {
 	{ import = "plugins.colorschemes.gruvbox" },
+
+	-- TODO: Remove after next release of LazyVim
+	-- https://github.com/LazyVim/LazyVim/pull/7124
+	{ "LazyVim/LazyVim", version = false },
 }
