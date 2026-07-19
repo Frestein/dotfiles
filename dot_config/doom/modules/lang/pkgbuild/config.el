90b0b4da1ab63201291b4f77614b4de1cb84a75c
Frestein <frestein@tuta.io>
2026-07-19 20:07:06 +0500

    feat(doom/makepkg): use comint-mode for makepkg process
    WIP, AI slop, PoC

--- dot_config/doom/modules/lang/pkgbuild/config.el
+++ dot_config/doom/modules/lang/pkgbuild/config.el
@@ -12,6 +12,61 @@
   :config
   (+pkgbuild-common-config 'pkgbuild-mode)
 
+  ;; TODO: WIP AI slop PoC
+  ;; INFO: Use comint-mode for makepkg process
+  (defvar pkgbuild-process-name "pkgbuild-makepkg")
+
+  (defun pkgbuild--buffer-name (command)
+    (format "*%s %s*" command (or buffer-file-name "PKGBUILD")))
+
+  (defun pkgbuild-makepkg (command)
+    "Use makepkg COMMAND to build package."
+    (interactive
+     (if pkgbuild-read-makepkg-command
+         (list (read-from-minibuffer "makepkg command: "
+                                     (eval pkgbuild-makepkg-command)
+                                     nil nil
+                                     'pkgbuild-makepkg-history))
+       (list (eval pkgbuild-makepkg-command))))
+    (save-some-buffers (not pkgbuild-ask-about-save) nil)
+    (unless (file-readable-p "PKGBUILD")
+      (error "No PKGBUILD in current directory"))
+    (let* ((bufname (pkgbuild--buffer-name command))
+           (buf (get-buffer-create bufname)))
+      (pkgbuild-process-check bufname)
+      (when (buffer-live-p buf)
+        (kill-buffer buf))
+      (setq buf (get-buffer-create bufname))
+      (with-current-buffer buf
+        (let ((inhibit-read-only t))
+          (erase-buffer)
+          (comint-mode)
+          (setq-local comint-prompt-regexp "^[^$#\n]*[#$%>] *")
+          (setq-local comint-use-prompt-regexp t)
+          (add-hook 'comint-output-filter-functions
+                    #'comint-watch-for-password-prompt nil t))
+        (display-buffer buf))
+      (let ((proc (start-process-shell-command "makepkg" bufname command)))
+        (set-process-filter proc #'comint-output-filter)
+        (set-process-sentinel proc #'pkgbuild-process-sentinel)
+        (with-current-buffer buf
+          (setq-local comint-ptyp proc))
+        proc)))
+
+  (defun pkgbuild-process-sentinel (process event)
+    (let ((buf (process-buffer process)))
+      (when (buffer-live-p buf)
+        (with-current-buffer buf
+          (let ((inhibit-read-only t))
+            (goto-char (point-max))
+            (insert event))))))
+
+  (defun pkgbuild-abort ()
+    (interactive)
+    (let ((proc (get-buffer-process (current-buffer))))
+      (when (and proc (process-live-p proc))
+        (interrupt-process proc))))
+
   (map! :map pkgbuild-mode-map
         :localleader
         "s" #'pkgbuild-update-sums-line
