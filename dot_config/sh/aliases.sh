f37356d916b418b327a4ceea934343194a249b29
Frestein <frestein@tuta.io>
2026-08-05 02:42:08 +0500

    chore(bash): drop tgeraser

--- dot_config/sh/aliases.sh
+++ dot_config/sh/aliases.sh
@@ -13,7 +13,6 @@ esac
 # ----------------------------------------------------------------------------
 alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
 alias yarn='yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config"'
-alias tgeraser='tgeraser -d "$XDG_DATA_HOME/tgeraser"'
 
 # ----------------------------------------------------------------------------
 # Quick aliases
