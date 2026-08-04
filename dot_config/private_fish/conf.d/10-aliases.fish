a73fb6d70281f8b1d376e1201d96aafeb7b1e840
Frestein <frestein@tuta.io>
2026-08-05 02:42:33 +0500

    chore(fish): drop tgeraser

--- dot_config/private_fish/conf.d/10-aliases.fish
+++ dot_config/private_fish/conf.d/10-aliases.fish
@@ -12,4 +12,3 @@ end
 # ----------------------------------------------------------------------------
 alias wget 'wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
 alias yarn 'yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config"'
-alias tgeraser 'tgeraser -d "$XDG_DATA_HOME/tgeraser"'
--- dot_config/private_fish/functions/tgeraser.fish
+++
@@ -1,3 +0,0 @@
-function tgeraser -w tgeraser -d 'alias tgeraser=tgeraser'
-    command tgeraser -d "$XDG_DATA_HOME/tgeraser" $argv
-end
