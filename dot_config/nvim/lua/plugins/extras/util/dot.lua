aeb268161e3ed0e532026841f5fb38584e4e6b75
Frestein <frestein@tuta.io>
2026-08-18 16:14:14 +0500

    chore(nvim): nvim-treesitter: remove `utils.fs` require and unused `add` function

--- dot_config/nvim/lua/plugins/extras/util/dot.lua
+++ dot_config/nvim/lua/plugins/extras/util/dot.lua
@@ -1,17 +1,9 @@
-local fs = require "utils.fs"
-
 return {
 	"nvim-treesitter/nvim-treesitter",
 	optional = true,
 	recommended = true,
 	desc = "Language support for dotfiles",
 	opts = function(_, opts)
-		local function add(lang)
-			if type(opts.ensure_installed) == "table" then
-				table.insert(opts.ensure_installed, lang)
-			end
-		end
-
 		vim.filetype.add {
 			pattern = {
 				[".*/.*%.hl"] = "hyprlang",
