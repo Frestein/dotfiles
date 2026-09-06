feb775cc3f1947320f35bd2ef3236aef04011883
Frestein <frestein@tuta.io>
2026-09-07 03:03:50 +0500

    chore(fish): functions: drop digga

--- home/dot_config/private_fish/functions/digga.fish
+++
@@ -1,7 +0,0 @@
-function digga -d 'Run `dig` and display the most useful info'
-    if test (count $argv) -eq 0
-        echo "Usage: digga <domain>"
-        return 1
-    end
-    dig +nocmd $argv[1] any +multiline +noall +answer
-end
