718555b6694d1cd3e0031677d3a9eb93dc573aac
Frestein <frestein@tuta.io>
2026-09-21 11:24:24 +0500

    chore(nvim): LazyVim: use latest release instead of master

--- home/dot_config/nvim/lua/plugins/init.lua
+++ home/dot_config/nvim/lua/plugins/init.lua
@@ -1,7 +1,3 @@
 return {
 	{ import = "plugins.colorschemes.gruvbox" },
-
-	-- TODO: Remove after next release of LazyVim
-	-- https://github.com/LazyVim/LazyVim/pull/7124
-	{ "LazyVim/LazyVim", version = false },
 }
